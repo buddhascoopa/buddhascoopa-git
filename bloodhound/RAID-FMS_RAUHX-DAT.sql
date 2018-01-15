a2b55a7a95e49c57f5da79009e345f9216e9745f
L. Frossard <lmfrossard@msn.com>
2018-01-15 11:39:55 -0200

    up 201801151139

---
+++ bloodhound/RAID-FMS_RAUHX-DAT.sql
@@ -0,0 +1,6 @@
+select * from fms_t_jec 
+select * from fms_t_jec_err 
+select * from fms_t_failed_events
+select * from fms_t_sinn_actv_oitv
+select * from fms_t_sinn_crm_contrato
+
---
+++ bloodhound/RAID-FMS_RAUHX-DAT_1.sql
@@ -0,0 +1,2 @@
+select * from fms_t_sinn_crm_contrato
+select * from fms_t_failed_events
